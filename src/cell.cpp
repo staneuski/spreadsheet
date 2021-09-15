c8173328929e6f7b18543a80c882adf6bce148eb
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2021-09-15 14:35:05 +0300

    Implement cell interface

---
+++ src/cell.cpp
@@ -0,0 +1,15 @@
+#include "cell.h"
+
+#include <cassert>
+
+void Cell::Set(std::string text) {
+    if (text.empty()) {
+        Clear();
+        return;
+    }
+
+    if (text.front() == FORMULA_SIGN && text.size() > 1u)
+        impl_ = std::make_unique<FormulaImpl>(text.substr(1));
+    else
+        impl_ = std::make_unique<TextImpl>(text);
+}
